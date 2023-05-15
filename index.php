ff72a7aac30cf5e366828413ccfba99133dabccc
adham <adham4282@gmail.com>
2023-05-14 20:53:00 -0500

    s

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>no quiere funcar</title>
 </head>
 <body>
-    <h2>hola5!</h2>
+    <h2>no funco</h2>
    
 </body>
 </html>
@@ -46,11 +46,5 @@ if($conexion){
 else {
     echo 'valio';
 }
-pg_close($conexion);
+
 ?>
-<?php
-
-session_start();
-                
-
-                ?>
