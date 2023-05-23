5592b4b1df80fe1ce663c803871e5c7ee2431018
adham <adham4282@gmail.com>
2023-05-23 14:55:51 -0500

    up13

--- index.php
+++ index.php
@@ -8,12 +8,13 @@ $user = new User();
 
 if(isset($_SESSION['user'])){
     echo "hay sesion";
-}else if (isset($_POST['username']) && isset($_POST['posword'])){
+}else if (isset($_POST['username']) && isset($_POST['password'])){
     $userForm = $_Post['username'];
     $passForm = $_Post['password'];
     if ($user->userExists($userForm,$passForm)){
         echo"usuairo invalido";
     }else{
+        echo"joder";
         $errorLogin = "Nombre de usuario y/o Contraseña incorrecto";
         include_once 'login.php';
     }
