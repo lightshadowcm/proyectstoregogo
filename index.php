7b72371d87c3e674a4acf1c956c6673f7fa85abd
adham <adham4282@gmail.com>
2023-05-23 15:07:11 -0500

    up15

--- index.php
+++ index.php
@@ -8,11 +8,16 @@ $user = new User();
 
 if(isset($_SESSION['user'])){
     echo "hay sesion";
+    $user->setUser($userSession->getCurrentUser() );
+    include_once 'home.php';
 }else if (isset($_POST['username']) && isset($_POST['password'])){
     $userForm = $_POST['username'];
     $passForm = $_POST['password'];
     if ($user->userExists($userForm,$passForm)){
-        echo"usuairo invalido";
+        echo"usuario valido";
+        $userSession->setCurrentUser($userForm);
+        $user->setUser($userForm);
+        include_once 'home.php';
     }else{
         echo"joder";
         $errorLogin = "Nombre de usuario y/o Contraseña incorrecto";
