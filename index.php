8adbf8921ab7a255214aa185a51b199cc422a6d5
adham <adham4282@gmail.com>
2023-05-23 14:30:49 -0500

    up7

--- index.php
+++ index.php
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-
-<body>
-     
-
-</body>
 <?php
-require_once 'conexion.php';
-$conexionDB = new ConexionDB();
-$conexion = $conexionDB->obtenerConexion();
-$query = "SELECT id FROM clientes";
-$result = pg_query($conexion, $query);
 
-if (!$result) {
-    echo "Error en la consulta: " . pg_last_error();
-    exit;
+include_once 'user.php';
+include_once 'user_session.php';
+
+$userSession = new userSession();
+$user = new User();
+
+if(isset($_SESSION['user'])){
+    echo "hay sesion";
+}else if (isset($_POST['username']) && isset($_POST['posword'])){
+ echo "validacion login";
+}
+else {
+    echo "login";
 }
 
-while ($row = pg_fetch_assoc($result)) {
-    echo "Campo 1: " . $row['id'] . "<br>";
-}
+
 
 ?>
-</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <h2>iniciar sesion</h2>
+        <label for="username">nombre usuario</label>
+        <input type="text" name="username">
+        <label for="password">paswword</label>
+        <input type="password" name="password" id="password">
+        <input type="submit" value="Iniciar sesion">
+
+    </form>
+</body>
+
+</html>
