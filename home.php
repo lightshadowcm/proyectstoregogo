d685853e03a381b3102021ba47f938a16431470a
adham <adham4282@gmail.com>
2023-11-08 14:04:55 -0500

    cv

--- home.php
+++ home.php
@@ -15,12 +15,10 @@
       <h1>El Derretido</h1>
       <nav>
         <ul class="ul-nav">
-          <li class="il-nav"><a class="a-nav" href="#">Incio</a></li>
+          <li class="il-nav"><a id="a-nav" href="#">Incio</a></li>
           
-          <li class="cerrar-sesion" class="il-nav">
-                <a href="/logout.php">Cerrar Sesion</a>
-            </li>
-          <li class="il-nav"><a class="a-nav" href="perfil.html">Perfil</a></li>
+          <li class="cerrar-sesion" id="il-nav"><a href="/logout.php">Cerrar Sesion</a></li>
+          <li class="il-nav"><a id="a-nav" href="perfil.html">Perfil</a></li>
         </ul>
       </nav>
       
