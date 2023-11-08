15c4c2426f2be10248fe0455ea2c4848053a342c
adham <adham4282@gmail.com>
2023-11-08 12:27:18 -0500

    ca

--- home.php
+++ home.php
@@ -1,43 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
+
 <body>
-    <header> 
+    <header>
+        <div><img src="icon" alt=""> <h1>Bienvenido a la tienda GoGo</h1></div>
         <ul>
-            <li>home</li>
+            <li><a href="#"></a></li>
             <li class="cerrar-sesion">
                 <a href="/logout.php">Cerrar Sesion</a>
-                <a href="/user.php">Cerrar Sesion</a>
             </li>
         </ul>
-        
-    </header>
-    <h1>Bienvenido a la tienda GoGo</h1>
-    <p>Estamos en construccion</p> <br> <br>
-    <article>
-     <label for="pedro bailando ">Pedro Laburando </label>
-     <iframe width="560" height="315" src="https://www.youtube.com/embed/sZVLcsLkEUk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; mute=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </article>
-    <article>
-        <label for="">Leandro Laburando</label> <iframe width="560" height="315" src="https://www.youtube.com/embed/MoO0WF95nsM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-    </articlea>
-    <article>
-        <label for="">iory buscando grupo</label>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/n5vhrk0Wed0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </article>
-    <article>
-        <label for="">adhan el dia mas tranquilo</label>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ysdPb1QSJSE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </article>
-    <article>
-        <label for="">maria</label>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/uWfbR_juSdY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;mute; picture-in-picture; web-share" allowfullscreen></iframe>
-    </article>
+    </header>
+    
+    <main>
+        
+    </main>
 </body>
+
 </html>
